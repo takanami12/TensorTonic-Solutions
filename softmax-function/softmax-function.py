8d26327c65bb0382e35eebcd3fdda83648115874
takanami12 <120194783+takanami12@users.noreply.github.com>
2026-03-13 11:36:52 +0700

    Add solution for softmax-function

---
+++ softmax-function/softmax-function.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def softmax(x):
+    """
+    Compute the softmax of input x.
+    Works for 1D or 2D NumPy arrays.
+    For 2D, compute row-wise softmax.
+    """
+    # Write code here
+    x = np.asarray(x)
+    if x.ndim == 1:
+        x_stable = x - np.max(x)
+        exp_x = np.exp(x_stable)
+        return exp_x / np.sum(exp_x)
+    else:
+        x_stable = x - np.max(x, axis=1, keepdims=True)
+        exp_x = np.exp(x_stable)
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
