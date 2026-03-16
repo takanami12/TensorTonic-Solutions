f72960667cb65e06589bdbaa617a344ea8dad3d8
takanami12 <120194783+takanami12@users.noreply.github.com>
2026-03-16 15:50:22 +0700

    Add solution for covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """
+    Compute covariance matrix from dataset X.
+    """
+    # Write code here
+    X = np.asarray(X, dtype=float)
+    
+    mu = np.mean(X, axis=0)
+    X_cen = X - mu
+
+    if X.ndim == 1 or X.shape[0] == 1:
+        return None
+    else:
+        cov = 1 / (X.shape[0] - 1) * X_cen.T @ X_cen
+
+        return cov
